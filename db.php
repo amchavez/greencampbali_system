e2dc7326055ee49d0a0482c49a4d1911a6a9657c
amchavez <37782550+amchavez@users.noreply.github.com>
2018-03-25 22:35:23 +0000

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'bali123';
-$db = 'greencampbali';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
